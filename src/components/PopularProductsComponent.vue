43278a9a0e1bd5d870b8705e914267a43110c93c
YaniekVanGenechtenPXL <yaniekvangenechten@student.pxl.be>
2023-12-22 19:13:10 +0100

    first commit, overzicht af

---
+++ src/components/PopularProductsComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="container">
+    <h2>Featured products</h2>
+    <ProductCardComponent
+      v-bind:products="featuredProducts"
+    ></ProductCardComponent>
+  </div>
+</template>
+
+<script>
+import ProductCardComponent from "./ProductCardComponent.vue";
+import products from "../products.json";
+
+export default {
+  components: {
+    ProductCardComponent,
+  },
+  data() {
+    return {
+      products: products,
+    };
+  },
+  computed: {
+    featuredProducts() {
+      return this.products.filter((product) =>
+        ["2", "7", "14"].includes(product.id)
+      );
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+#container {
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+#flex {
+  display: inline-block;
+}
+h2 {
+  width: 100%;
+  text-align: center;
+  color: #899d5e;
+}
+</style>
---
+++ src/components/ProductCardComponent.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="overview__box hair">
+    <div><img :src="product.image" :alt="product.title" /></div>
+    <h3>{{ product.title }}</h3>
+    <p>€{{ product.price }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: Object
+  }
+}
+</script>
+
+<style scoped>
+div.overview__box {
+  color: #899d5e;
+  margin-bottom: 5vh;
+}
+div.overview__box a {
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  text-decoration: none;
+  color: #899d5e;
+}
+div.overview__box div {
+  width: 100%;
+  height: 250px;
+  background-color: #fff;
+  border: 2px solid #2b7180;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+}
+div.overview__box div:hover img {
+  width: 90%;
+}
+div.overview__box div img {
+  width: 80%;
+  transition: ease-in-out 0.5s;
+}
+div.overview__box p {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
+  font-size: auto;
+}
+div.overview__box p span {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 400;
+  font-size: auto;
+  margin-right: 20px;
+  text-decoration: line-through #f00;
+}
+</style>
